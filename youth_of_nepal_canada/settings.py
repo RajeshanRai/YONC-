b25ce88f77948ea469ef9b8c2ddf58bd9edebd68
Rajeshan Rai <yellowtown85@gmail.com>
2026-06-17 15:19:08 -0700

    first commit

--- youth_of_nepal_canada/settings.py
+++ youth_of_nepal_canada/settings.py
@@ -140,7 +140,7 @@ MESSAGE_TAGS = {
 DEFAULT_FROM_EMAIL = 'rajeshanrai.official@gmail.com'
 EMAIL_BACKEND = os.environ.get('DJANGO_EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = os.environ.get('DJANGO_EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('DJANGO_EMAIL_PORT'))
+EMAIL_PORT = os.environ.get('DJANGO_EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('DJANGO_EMAIL_USE_TLS') == 'True'
